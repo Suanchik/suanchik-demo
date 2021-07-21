f17ff34080280fe5db94bf8c34b4e56fa14c2785
Suanchik <tamu.suanchik@bk.ru>
2021-07-21 14:43:43 +0300

    Updates

---
+++ precache-manifest.c3e98efb5486feb50d8c0fafbfe5e9ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec622ea27d81141fce720a6d2887372",
+    "url": "./https://suanchik.github.io/suanchik-demo/index.html"
+  },
+  {
+    "revision": "778950ea6b497cbccbb1",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/css/3.bce4f03a.chunk.css"
+  },
+  {
+    "revision": "a95c145912886e76854f",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/css/main.fa9ed4c3.chunk.css"
+  },
+  {
+    "revision": "4ff4f28ae1b41bf08789",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/js/2.609d95d0.chunk.js"
+  },
+  {
+    "revision": "eafe05020a9e8d5ac192526352c83990",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/js/2.609d95d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778950ea6b497cbccbb1",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/js/3.1ae40eb3.chunk.js"
+  },
+  {
+    "revision": "a95c145912886e76854f",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/js/main.7d44d4f6.chunk.js"
+  },
+  {
+    "revision": "817599f96f9f7e296769",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/js/runtime-main.291fd344.js"
+  },
+  {
+    "revision": "c101106007404983139f7e50fd4481aa",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/media/loading.c1011060.gif"
+  },
+  {
+    "revision": "80dc001f6474bba7a455fc5a82148f4a",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/media/mal.80dc001f.jpg"
+  },
+  {
+    "revision": "765bb451a9422fad476cd62270a6440f",
+    "url": "./https://suanchik.github.io/suanchik-demo/static/media/mal2.765bb451.jpg"
+  }
+]);
