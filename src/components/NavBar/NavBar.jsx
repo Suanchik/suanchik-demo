2a6a8fefafeb809d2878b652ced8073509ac297d
Suanchik <tamu.suanchik@bk.ru>
2021-07-26 21:20:53 +0300

    cganged NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
     return (
         <nav className={classes.Nav}>
             <ul>
-                <li><NavLink to="/Profile" activeClassName={classes.activeLink}><div className={classes.item}>Профиль</div></NavLink></li>
+                <li><NavLink to="/Profile" activeClassName={classes.activeLink}><div className={classes.item}>Мой профиль</div></NavLink></li>
                 <li><NavLink to="/Messenger" activeClassName={classes.activeLink}><div className={classes.item}>Сообщения</div></NavLink></li>
                 <li><NavLink to="/People" activeClassName={classes.activeLink}><div className={classes.item}>Люди</div></NavLink></li>
                 {/* <li><NavLink to="/News" activeClassName={classes.activeLink}><div className={classes.item}>Новости</div></NavLink></li>
